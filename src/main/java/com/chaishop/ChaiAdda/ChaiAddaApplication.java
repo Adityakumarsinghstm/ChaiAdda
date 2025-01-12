a6652100a5241da405095e4f45904edd19268244
Aditya <adityasingh62065@gmail.com>
2025-01-12 20:16:28 +0530

    setup done

---
+++ src/main/java/com/chaishop/ChaiAdda/ChaiAddaApplication.java
@@ -0,0 +1,13 @@
+package com.chaishop.ChaiAdda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaiAddaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaiAddaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaishop/ChaiAdda/ChaiAddaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaishop.ChaiAdda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaiAddaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
